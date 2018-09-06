94458df8c818ded34b0111b5d1ca6a25dde5169e
xiaolin <462441355@qq.com>
2018-09-06 18:06:23 +0800

    * 封装response

---
+++ src/Basic/Response.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/9/6
+ * Time: 下午6:04
+ */
+
+namespace Lin\Src\Basic;
+
+/**
+ * Class Response
+ * @package Lin\Src\Basic
+ */
+class Response
+{
+    public $code = 0;
+
+    public $request;
+
+    public $response = [];
+
+    /**
+     * 发送请求
+     * @return array
+     */
+    public function sendResponse()
+    {
+
+        if ($this->code == 200) {
+            return $this->success([], $this->response);
+        }
+
+        if ($this->code == 300) {
+            return $this->error([], $this->response);
+        }
+
+        if ($this->code == 500) {
+            return $this->fail([], $this->response);
+        }
+    }
+
+    /**
+     * 请求成功
+     * @param array $request
+     * @param array $response
+     * @return array
+     */
+    public function success($request = [], $response = [])
+    {
+        return api_response($response, 0);
+    }
+
+
+    /**
+     * 请求失败
+     * @param array $request
+     * @param array $response
+     * @return array
+     */
+    public function fail($request = [], $response = [])
+    {
+        $message = array_get($response, 'errorMessage', null);
+        return api_response($response, 500, $message);
+    }
+
+    /**
+     * 缺少参数
+     * @param array $request
+     * @param array $response
+     * @return array
+     */
+    public function error($request = [], $response = [])
+    {
+        return api_response($response, 300, '缺少参数');
+    }
+
+    /**
+     * @param array $request
+     * @param array $response
+     * @return array
+     */
+    public function callback($request = [], $response = [])
+    {
+        return $this->success($request, $response);
+    }
+}
