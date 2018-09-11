7eb288f71683e5762f186a04bc8e63da9f326665
xiaolin <462441355@qq.com>
2018-09-11 14:12:59 +0800

    * 增加逻辑层

--- src/Basic/Client.php
+++ src/Basic/Client.php
@@ -19,7 +19,7 @@ class Client
      * @param object $response
      * @param object $manager
      * @param $data array
-     * @return string
+     * @return mixed
      */
     public function run($form, $request, $response, $manager, $data = [])
     {
@@ -38,6 +38,30 @@ class Client
                 ];
             }
             return $response->fail($data, $result);
+        }
+        return $response->callback($data, $result);
+    }
+
+    /**
+     * @param $form object
+     * @param $logic object
+     * @param $response object
+     * @param $data array
+     * @return mixed
+     */
+    public function doRun($form, $logic, $response, $data = [])
+    {
+        $data = $form->validate($data);
+        try {
+            $result = $logic->run($data);
+        } catch (Exception $ex) {
+            $error = [
+                'url' => request()->url(),
+                'method' => request()->getMethod(),
+                'data' => request()->all(),
+                'error' => $ex->getMessage(),
+            ];
+            return $response->fail($data, $error);
         }
         return $response->callback($data, $result);
     }
